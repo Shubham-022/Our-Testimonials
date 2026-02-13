17b805b44434c9f7eabc3cc5352a61b6c99d9283
Shubham-022 <shubhambavistale859@gmail.com>
2026-02-13 08:42:09 +0530

    fixes app.js error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Card from "./components/Card";
-import data from "./data";
 import { useState } from "react";
 
 const App = () => {
